c50b02ca7857c16598c0e8b92d6198050f82e427
Adrian <AdrianGtrejo@gmail.com>
2017-07-28 22:47:37 -0500

    Made progress

--- all_star1.5.py
+++ all_star1.5.py
@@ -13,74 +13,512 @@ buzz_pin = 32
 GPIO.setup(buzz_pin,GPIO.OUT)
 
 #Notes with pitches
-D = GPIO.PWM(buzz_pin,293.66)
-C = GPIO.PWM(buzz_pin,261.63)
-B = GPIO.PWM(buzz_pin,246.94)
-A = GPIO.PWM(buzz_pin,220)
-G = GPIO.PWM(buzz_pin,196)
+D = 293.66
+C = 261.63
+B = 246.94
+A = 220
+G = 196
+e = 164.8
+d = 146.83
 
 # Quarter
-Buzz = G
+Buzz = GPIO.PWM(buzz_pin,G)
 Buzz.start(50)
 time.sleep(1)
 Buzz.stop()
 time.sleep(0.02)
 
 # Eighth
-Buzz = D
+Buzz = GPIO.PWM(buzz_pin,D)
 Buzz.start(50)
 time.sleep(0.5)
 Buzz.stop()
-time.sleep(0.5)
+time.sleep(0.05)
 
 # Eighth
-Buzz = B
+Buzz = GPIO.PWM(buzz_pin,B)
 Buzz.start(50)
-time.sleep(0.05)
+time.sleep(0.5)
 Buzz.stop()
-time.sleep(1)
+time.sleep(0.05)
 
 # Quarter
-Buzz = B
+Buzz = GPIO.PWM(buzz_pin,B)
 Buzz.start(50)
 time.sleep(1)
 Buzz.stop()
 time.sleep(0.02)
 
 # Eighth
-Buzz = A
+Buzz = GPIO.PWM(buzz_pin,A)
 Buzz.start(50)
 time.sleep(0.5)
 Buzz.stop()
 time.sleep(0.05)
 
 # Eighth
-Buzz = G
+Buzz = GPIO.PWM(buzz_pin,G)
 Buzz.start(50)
 time.sleep(0.5)
 Buzz.stop()
 time.sleep(0.05)
 
 # Eighth
-Buzz = G
+Buzz = GPIO.PWM(buzz_pin,G)
 Buzz.start(50)
 time.sleep(0.5)
 Buzz.stop()
 time.sleep(0.05)
 
-# Eighth
-Buzz = C
+# Quarter
+Buzz = GPIO.PWM(buzz_pin,C)
 Buzz.start(50)
 time.sleep(1)
 Buzz.stop()
 time.sleep(0.02)
 
 # Eighth
-Buzz = B
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth rest
+time.sleep(0.2)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,D)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth slur
+Buzz = GPIO.PWM(buzz_pin,e)
+Buzz.start(50)
+time.sleep(1)
+Buzz.stop()
+time.sleep(0.05)
+
+# Half slur
+Buzz = GPIO.PWM(buzz_pin,d)
+Buzz.start(1.5)
+time.sleep(1)
+Buzz.stop()
+time.sleep(0.05)
+
+#Rest
+time.sleep(1)
+
+# Eighth she
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth was
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth look-
+Buzz = GPIO.PWM(buzz_pin,D)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth ing
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth kind
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth of
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth dumb
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth with
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth her
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Quarter fin-
+Buzz = GPIO.PWM(buzz_pin,C)
 Buzz.start(50)
 time.sleep(1)
 Buzz.stop()
 time.sleep(0.02)
+
+# Eighth - ger
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth and
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth her
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth thumb
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth 
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Quarter
+Buzz = GPIO.PWM(buzz_pin,D)
+Buzz.start(50)
+time.sleep(1)
+Buzz.stop()
+time.sleep(0.02)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Quarter
+Buzz = GPIO.PWM(buzz_pin,A)
+Buzz.start(50)
+time.sleep(1)
+Buzz.stop()
+time.sleep(0.02)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth slur
+Buzz = GPIO.PWM(buzz_pin,e)
+Buzz.start(50)
+time.sleep(1)
+Buzz.stop()
+time.sleep(0.05)
+
+# Half slur
+Buzz = GPIO.PWM(buzz_pin,d)
+Buzz.start(50)
+time.sleep(1)
+Buzz.stop()
+time.sleep(0.05)
+
+#Rest
+time.sleep(1)
+
+# Eight dotted
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.2)
+Buzz.stop()
+time.sleep(0.2)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,e)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.05)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Eight dotted
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.2)
+Buzz.stop()
+time.sleep(0.2)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,e)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.055)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.055)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.05)
+Buzz.stop()
+time.sleep(0.055)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.055)
+
+# Eighth
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.05)
+
+# Sixteenth
+Buzz = GPIO.PWM(buzz_pin,B)
+Buzz.start(50)
+time.sleep(0.5)
+Buzz.stop()
+time.sleep(0.055)
+
+# Eight dotted
+Buzz = GPIO.PWM(buzz_pin,G)
+Buzz.start(50)
+time.sleep(0.2)
+Buzz.stop()
+time.sleep(0.2)
 
 # reset GPIO resources used by script
 GPIO.cleanup()
